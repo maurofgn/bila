738ac250b2cb9fc42d33ffb9d855f969cdf1eda4
Mauro Fugante <mauro.fugante@gmail.com>
2015-09-21 13:07:41 +0200

    gestito l'anno e l'anno-1. Questo perchè jasper non mi permette di usare anno come integer, sicuramente il problema sarebbe risolvibile lato jasper, ma per non dedicargli tempo ho risolto lato server. Ho aggiunto anche un pezzo commentato per ottenere il pdf come ByteArrayOutputStream, nel caso specifico non serve, ma questo sarebbe utile nel caso si voglia spedire il pdf come allegato in una mail o necessità simili

--- grails-app/controllers/org/mesis/bila/JasperReportController.groovy
+++ grails-app/controllers/org/mesis/bila/JasperReportController.groovy
@@ -6,35 +6,20 @@ class JasperReportController {
 	
     /* shows the records listing page*/
     def index() {
-//        def jasperDTOList = bilaService.loadAllJasperDTO(20)
-//        render(view:"index", model:[jasperDTOList:jasperDTOList])
 		render(view:"index")
-//		redirect action: 'generateReport'
     }
 	
     /* serves the report document for download*/
     def generateReport() {
 		
-		Integer year =  params.year ? Integer.parseInt(params.year) : new Date()[Calendar.YEAR];
-				
-//		if (!year) {
-//			year = new Date()[Calendar.YEAR]
-//			params.year = year
-//		} else {
-//			Integer.parseInt(params.year);
-//		}
-	
-//parametri identificativi del template
+		if (!params.year) {
+			params.year = new Date()[Calendar.YEAR]
+		}
+		
+		Integer year = Integer.parseInt(params.year)	// ? Integer.parseInt(params.year) : new Date()[Calendar.YEAR]
+		params.year = year
+		params.yearPrev = year-1
 
-
-//_format=PDF&_name=all_dto&_file=all_dto
-
-//		params.format="PDF"
-////		params._name="bilancio.jrxml"
-//		params.name="bilancio_${year}"
-////		params._file="bilancio_${year}.PDF"
-//		params.file="bilancio"
-////////////
 //parametri del report
         params.title = "Bilancio"
         params.reportId = "Mesis srl via dei Velini 19 Macerata Italy"
@@ -52,10 +37,26 @@ class JasperReportController {
 		bilaRows += oneRootNode(NodeType.COSTI)
 		bilaRows += oneRootNode(NodeType.RICAVI)
 		
+		
+//		Map result = [:]
+//        result.data = []
+//        result.data = << [code:"vivek",description:"yadav"] // from here you can send any type of data  
+//		
+//		
+////        result.data = bilarows	//<< [name:”vivek”,surName:”yadav”] // from here you can send any type of data  
+//  // what ever you want
+//        JasperReportDef rep = jasperService.buildReportDefinition(params, request.locale, result)
+//        ByteArrayOutputStream stream = jasperService.generateReport(rep)
+//        response.setHeader("Content-disposition", "attachment; filename=" + 'fileName' + ".pdf")
+//        response.contentType = "application/pdf"
+//        response.outputStream << stream.toByteArray()
+		
 		//chiamata al controller jasper
         chain(controller:'jasper', action:'index',
             model:[data:bilaRows],
             params:params)
+			
+//		render(view:"index")
         return false
     }
 }
