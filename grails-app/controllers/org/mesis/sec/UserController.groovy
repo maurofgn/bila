399ee93c16d18df2a4e2bfea0917907bfa44d9eb
Mauro Fugante <mauro.fugante@gmail.com>
2015-09-21 17:35:00 +0200

    user

---
+++ grails-app/controllers/org/mesis/sec/UserController.groovy
@@ -0,0 +1,136 @@
+package org.mesis.sec
+
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class UserController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+	
+//	def beforeInterceptor = [action:this.&auth, except:["index", "list", "show"]]
+
+	def auth() {
+		if(!session.user) {
+		  redirect(controller:"user", action:"login")
+		  return false
+		}
+	}
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond User.list(params), model:[userInstanceCount: User.count()]
+    }
+
+    def show(User userInstance) {
+        respond userInstance
+    }
+
+    def create() {
+        respond new User(params)
+    }
+	
+	def login = {}
+
+	def authenticate = {
+		def user = User.findByLoginAndPassword(params.login, params.password)
+		if(user){
+			  session.user = user
+			  flash.message = "Hello ${user.name}!"
+//			  redirect(controller:"entry", action:"list")
+			  redirect(uri: "/")
+			}else{
+			  flash.message = "Sorry, ${params.login}. Please try again."
+			  redirect(action:"login")
+			}
+	}
+
+	def logout = {
+		flash.message = "Goodbye ${session.user.name}"
+		session.user = null
+//		redirect(controller:"entry", action:"list")
+		 redirect(uri: "/")
+	}	
+	
+
+    @Transactional
+    def save(User userInstance) {
+        if (userInstance == null) {
+            notFound()
+            return
+        }
+
+        if (userInstance.hasErrors()) {
+            respond userInstance.errors, view:'create'
+            return
+        }
+
+        userInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])
+                redirect userInstance
+            }
+            '*' { respond userInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(User userInstance) {
+        respond userInstance
+    }
+
+    @Transactional
+    def update(User userInstance) {
+        if (userInstance == null) {
+            notFound()
+            return
+        }
+
+        if (userInstance.hasErrors()) {
+            respond userInstance.errors, view:'edit'
+            return
+        }
+
+        userInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'User.label', default: 'User'), userInstance.id])
+                redirect userInstance
+            }
+            '*'{ respond userInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(User userInstance) {
+
+        if (userInstance == null) {
+            notFound()
+            return
+        }
+
+        userInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'User.label', default: 'User'), userInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
